e155317710fe279f7bbb067d6e8f6661373770cc
Neel <ezebarazarte@gmail.com>
2025-01-24 14:32:37 -0400

    redondeado blanco

--- botonHero/src/components/CreditoInmediato.jsx
+++ botonHero/src/components/CreditoInmediato.jsx
@@ -255,7 +255,7 @@ const CreditoInmediato = () => {
 
         <div className="flex-1">
             <div className="w-80 rounded-3xl mx-auto overflow-hidden "> {/* shadow-xl */}
-                <div className="px-10 pt-4 pb-8 rounded-tr-4xl">
+                <div className="px-10 pt-4 bg-white pb-8 rounded-tr-4xl">
                     {/* <h1 className="text-2xl font-semibold text-gray-900">Crédito Inmediato</h1> */}
 
                     {loading ? (
