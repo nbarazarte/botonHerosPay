74d18ec4b4f5922d6a360d908d63342695fe2fdc
Neel <ezebarazarte@gmail.com>
2025-01-28 16:56:02 -0400

    menu

--- botonHero/src/App.jsx
+++ botonHero/src/App.jsx
@@ -1,23 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas as solidIcons } from '@fortawesome/free-solid-svg-icons'
-import { fab as brandIcons } from '@fortawesome/free-brands-svg-icons'
-import { far as regularIcons } from '@fortawesome/free-regular-svg-icons'
-library.add(solidIcons, brandIcons, regularIcons) // Añade conjuntos de íconos 
-import CreditoInmediato from './components/CreditoInmediato'
-import DebitoInmediato from './components/DebitoInmediato'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import './styles.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas as solidIcons } from '@fortawesome/free-solid-svg-icons';
+import { fab as brandIcons } from '@fortawesome/free-brands-svg-icons';
+import { far as regularIcons } from '@fortawesome/free-regular-svg-icons';
+library.add(solidIcons, brandIcons, regularIcons); // Añade conjuntos de íconos
+import CreditoInmediato from './components/CreditoInmediato';
+import DebitoInmediato from './components/DebitoInmediato';
 
 function App() {
-
   return (
-    <>
-      {/* <CreditoInmediato /> */}
-      <DebitoInmediato />
-    </>
-  )
+    <Router basename="/boton">
+      <div>
+        <nav>
+          <ul className="menu">
+            <li>
+              <Link to="/credito-inmediato">Crédito Inmediato</Link>
+            </li>
+            <li>
+              <Link to="/debito-inmediato">Débito Inmediato</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/credito-inmediato" element={<CreditoInmediato />} />
+          <Route path="/debito-inmediato" element={<DebitoInmediato />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
