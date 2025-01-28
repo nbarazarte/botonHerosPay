f8afa244457a74376322312995c4a5a71ce696db
Neel <ezebarazarte@gmail.com>
2025-01-28 10:49:10 -0400

    base en vite.config

--- botonHero/vite.config.js
+++ botonHero/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: 'REACT_APP_'
+  envPrefix: 'REACT_APP_',
+  base: '/boton/'
 })
