2091112f4223f924a73a92e923eb7418cb934fab
Neel <ezebarazarte@gmail.com>
2025-01-29 12:31:21 -0400

    pass bd

--- apiBotonHero/db.js
+++ apiBotonHero/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',          // Tu usuario
-    host: 'localhost',         // El host donde se encuentra tu base de datos
-    database: 'herostokendb',       // Nombre de tu base de datos
-    password: '', // Tu contraseña
-    port: 5432,                // Puerto de PostgreSQL (por defecto 5432)
+    user: 'postgres',           // Tu usuario
+    host: 'localhost',          // El host donde se encuentra tu base de datos
+    database: 'herostokendb',   // Nombre de tu base de datos
+    password: 'H3r05*!',        // Tu contraseña
+    port: 5432,                 // Puerto de PostgreSQL (por defecto 5432)
 });
 
 module.exports = pool;
