2852aed8f597877b0b77cd37bad687b7c69bcec7
Neel <ezebarazarte@gmail.com>
2025-01-31 10:16:53 -0400

    select de bancos color blanco

--- botonHero/src/components/DebitoInmediato.jsx
+++ botonHero/src/components/DebitoInmediato.jsx
@@ -598,7 +598,7 @@ const CreditoInmediato = () => {
                                     <div className="flex flex-1 h-full justify-center items-center">
                                         <form className="mt-1" onSubmit={handleSubmitSinOtp}>
                                             <label htmlFor="bank" className="block">
-                                                <select value={selectedBank} onChange={handleSelectChange} className="pl-1 pr-1 w-56 mt-0 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-naranjaMove" id="bank">
+                                                <select value={selectedBank} onChange={handleSelectChange} className=" bg-white pl-1 pr-1 w-56 mt-0 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-naranjaMove" id="bank">
                                                     <option value="" disabled className='text-center'>Seleccione el Banco</option>
                                                     {bankOptions.map((bank) => (
                                                         <option key={bank.codigo_banco} value={bank.codigo_banco}>{`${bank.codigo_banco} - ${bank.nombre_banco}`}</option>
