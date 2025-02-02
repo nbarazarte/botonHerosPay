2110df91e47b51d74c38e2da083fa5e2be24a29f
Neil Barazarte <ezebarazarte@gmail.com>
2025-02-02 18:55:03 -0400

    ventana de aprobacion lista

--- botonHero/src/components/DebitoInmediato.jsx
+++ botonHero/src/components/DebitoInmediato.jsx
@@ -586,89 +586,65 @@ const DebitoInmediato = () => {
 
                             {token && (
 
-                                <>
+                                <div className="relative z-10">
+                                    <div
+                                        transition
+                                        className="fixed inset-0 bg-gray-300 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+                                    />
 
-                                    {/* <div className="flex justify-center items-center pt-24">
-                                    <div className="bg-green-100 border-t-4 border-green-500 rounded-b text-green-900 px-4 py-1 shadow-md w-60" role="alert">
+                                    <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                        <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+                                            <div
+                                                transition
+                                                className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                                            >
+                                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                                    <div className="sm:flex sm:items-start">
 
-                                        <div className="flex justify-center items-center text-center">
-                                            <div className='justify-center items-center' >
+                                                        <div className="mt-3 text-center">
+                                                            <div className="text-base font-semibold text-gray-900 justify-center items-center">
 
-                                                <div className="flex flex-row justify-center items-center gap-1">
-                                                    <Lottie animationData={paySuccess} loop={false} style={{ width: '20px', height: '20px' }} />
-                                                    <p className="text-2xl">¡Aprobado!</p>
-                                                </div>
-
-                                                <div className="flex flex-row justify-center items-center gap-1">
-                                                    <Lottie animationData={wifi} loop={true} style={{ width: '30px', height: '30px' }} />
-                                                    <p className="text-lg justify-center items-center">
-                                                        Token de Acceso: <span className='font-bold text-2xl'>{token.token}</span>
-                                                    </p>
-
-                                                    <button className="hover:text-black text-gray-400 text-center cursor-pointer" onClick={handleCopy}>
-                                                        <FontAwesomeIcon className='text-lg' icon="fa-regular fa-copy" />
-                                                    </button>
-                                                </div>
-
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div> */}
-
-                                    <div className="relative z-10">
-                                        <div
-                                            transition
-                                            className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
-                                        />
-
-                                        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                                            <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
-                                                <div
-                                                    transition
-                                                    className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
-                                                >
-                                                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                                        <div className="sm:flex sm:items-start">
-                                                            <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:size-10">
-                                                                <Lottie animationData={paySuccess} loop={false} style={{ width: '20px', height: '20px' }} />
-                                                            </div>
-                                                            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                                                <div className="text-base font-semibold text-gray-900">
-                                                                    ¡Pago Aprobado!
+                                                                <div className='flex flex-row justify-center items-center text-black gap-1'>
+                                                                    <div className="flex size-12 items-center justify-center rounded-full bg-green-100 ">
+                                                                        <Lottie animationData={paySuccess} loop={false} style={{ width: '20px', height: '20px' }} />
+                                                                    </div>
+                                                                    <p>¡Pago Aprobado!</p>
                                                                 </div>
-                                                                <div className="mt-2">
-                                                                    <p className="text-sm text-gray-500">
-                                                                        Por favor, copie el token de acceso asignado en la casilla <span className='font-bold text-black'>Token *</span> que aparece en la parte inferior de esta pantalla para conectarse a la red.
-                                                                    </p>
-                                                                </div>
-                                                            </div>
 
+
+                                                            </div>
+                                                            <div className="mt-2">
+                                                                <p className="text-sm text-gray-500">
+                                                                    Por favor, copie el token de acceso asignado en la casilla <span className='font-bold text-black'>Token *</span> que aparece en la parte inferior de esta pantalla para conectarse a la red.
+                                                                </p>
+                                                            </div>
                                                         </div>
 
-                                                        <div className="flex flex-row justify-center items-center">
+                                                    </div>
+
+                                                    <div className="flex flex-row justify-center items-center">
+                                                        <div className="justify-center items-center">
                                                             <Lottie animationData={wifi} loop={true} style={{ width: '30px', height: '30px' }} />
-                                                            <p className="text-lg justify-center items-center">
-                                                                Token de Acceso: <span className='font-bold text-2xl'>{token.token}</span>
-                                                            </p>
                                                         </div>
+                                                        <p className="justify-center items-center">
+                                                            Token de Acceso: <span className='font-bold'>{token.token}</span>
+                                                        </p>
                                                     </div>
-                                                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                                        <button
-                                                            type="button"
-                                                            onClick={handleCopy}
-                                                            className="inline-flex w-full justify-center rounded-md bg-naranjaMove px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-orange-300 sm:ml-3 sm:w-auto"
-                                                        >
-                                                            Copiar Token
-                                                        </button>
+                                                </div>
+                                                <div className="bg-gray-50 py-3 flex flex-row-reverse px-6 justify-center items-center">
+                                                    <button
+                                                        type="button"
+                                                        onClick={handleCopy}
+                                                        className="inline-flex  justify-center rounded-md bg-naranjaMove px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-orange-300 sm:ml-3 sm:w-auto"
+                                                    >
+                                                        Copiar Token
+                                                    </button>
 
-                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-
-                                </>
+                                </div>
 
                             )}
                         </>
@@ -780,7 +756,7 @@ const DebitoInmediato = () => {
 
                                             <div className='pb-2'>
                                                 <button type="submit" className="mt-10 px-4 py-2 rounded-xl bg-azulMove text-white font-sans font-semibold text-sm text-center block w-full cursor-pointer">
-                                                    {/* SOLICITAR OTP */} ENVIAR DATOS DE PAGO
+                                                    ENVIAR DATOS DE PAGO
                                                 </button>
                                             </div>
 
@@ -817,15 +793,16 @@ const DebitoInmediato = () => {
                                                         </>
                                                     )}
 
-                                                    <div className="mt-8 relative flex flex-row pl-1 pr-1">
-                                                        <input id="otp" type="number"
-                                                            value={otp}
-                                                            onChange={handleChangeOtp}
-                                                            onInput={(e) => { e.target.value = e.target.value.slice(0, 8) }}
-                                                            className="w-56 peer h-10 border-b-1 border-azulMove text-gray-900 placeholder-transparent focus:outline-none focus:border-naranjaMove" />
-                                                        <label htmlFor="otp" className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Escriba o pegue su código OTP</label>
-                                                    </div>
-
+                                                    {isVisible && (
+                                                        <div className="mt-8 relative flex flex-row pl-1 pr-1">
+                                                            <input id="otp" type="number"
+                                                                value={otp}
+                                                                onChange={handleChangeOtp}
+                                                                onInput={(e) => { e.target.value = e.target.value.slice(0, 8) }}
+                                                                className="w-56 peer h-10 border-b-1 border-azulMove text-gray-900 placeholder-transparent focus:outline-none focus:border-naranjaMove" />
+                                                            <label htmlFor="otp" className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Escriba o pegue su código OTP</label>
+                                                        </div>
+                                                    )}
                                                     {!errorPago ? (
                                                         <div className='pb-2'>
                                                             {isVisible && (
@@ -837,6 +814,7 @@ const DebitoInmediato = () => {
                                                                 </button>
                                                             )}
                                                         </div>
+
                                                     ) : (
                                                         <div className='pb-2'>
                                                             <button onClick={() => window.location.reload()} className="mt-10 px-4 py-2 rounded-xl bg-azulMove text-white font-sans font-semibold text-sm text-center block w-full cursor-pointer">
