f8d43b0c1830b606c6b83f94577eaab82cb7c87f
Neel <ezebarazarte@gmail.com>
2025-02-03 11:30:12 -0400

    otros cambios

--- botonHero/src/components/DebitoInmediato.jsx
+++ botonHero/src/components/DebitoInmediato.jsx
@@ -793,17 +793,17 @@ const DebitoInmediato = () => {
                                                         </>
                                                     )}
 
-                                                    {isVisible && (
-                                                        <div className="mt-8 relative flex flex-row pl-1 pr-1">
-                                                            <input id="otp" type="number"
-                                                                value={otp}
-                                                                placeholder=""
-                                                                onChange={handleChangeOtp}
-                                                                onInput={(e) => { e.target.value = e.target.value.slice(0, 8) }}
-                                                                className="w-56 peer h-10 border-b-1 border-azulMove text-gray-900 placeholder-transparent focus:outline-none focus:border-naranjaMove" />
-                                                            <label htmlFor="otp" className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Ingrese el código recibido</label>
-                                                        </div>
-                                                    )}
+
+                                                    <div className="mt-8 relative flex flex-row pl-1 pr-1">
+                                                        <input id="otp" type="number"
+                                                            value={otp}
+                                                            placeholder=""
+                                                            onChange={handleChangeOtp}
+                                                            onInput={(e) => { e.target.value = e.target.value.slice(0, 8) }}
+                                                            className="w-56 peer h-10 border-b-1 border-azulMove text-gray-900 placeholder-transparent focus:outline-none focus:border-naranjaMove" />
+                                                        <label htmlFor="otp" className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Ingrese el código recibido</label>
+                                                    </div>
+
                                                     {!errorPago ? (
                                                         <div className='pb-2'>
                                                             {isVisible && (
