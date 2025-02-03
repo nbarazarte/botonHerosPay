2343e0b9d6af70b3107877ca00befc6416b2d6a9
Neel <ezebarazarte@gmail.com>
2025-02-03 15:01:14 -0400

    quite transition

--- botonHero/src/components/DebitoInmediato.jsx
+++ botonHero/src/components/DebitoInmediato.jsx
@@ -595,14 +595,14 @@ const DebitoInmediato = () => {
 
                                 <div className="relative z-10">
                                     <div
-                                        transition
+                                        //transition
                                         className="fixed inset-0 bg-gray-300 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
                                     />
 
                                     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                                         <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
                                             <div
-                                                transition
+                                                //transition
                                                 className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
                                             >
                                                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
