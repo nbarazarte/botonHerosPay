0faaf267b96e60a03c9031de01d70a6c20a910ab
Neel <ezebarazarte@gmail.com>
2025-02-03 15:08:00 -0400

    lg al titulo

--- botonHero/src/components/DebitoInmediato.jsx
+++ botonHero/src/components/DebitoInmediato.jsx
@@ -587,7 +587,7 @@ const DebitoInmediato = () => {
                             ) : (
 
                                 <div className="justify-center items-center text-center pt-3 pb-3">
-                                    <h1 className="text-2xl">Pago Débito Inmediato</h1>
+                                    <h1 className="text-lg">Pago Débito Inmediato</h1>
                                 </div>
                             )}
 
