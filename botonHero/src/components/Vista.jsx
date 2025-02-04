8ba50ebdcc59e945e083327b0292a966a9505039
Neel <ezebarazarte@gmail.com>
2025-02-04 16:59:08 -0400

    estilos a la vista de consulta

--- botonHero/src/components/Vista.jsx
+++ botonHero/src/components/Vista.jsx
@@ -4,6 +4,8 @@ import { DataGrid } from '@mui/x-data-grid';
 import { Box, Button, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import * as XLSX from 'xlsx';
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const DataTable = () => {
     const urlApiBoton = import.meta.env.REACT_APP_URL_API_BOTON_SERVIDOR_PUBLICO;
@@ -248,11 +250,17 @@ const DataTable = () => {
     };
 
     return (
+
         <Box sx={{ height: 700 }} className="p-4 w-full">
-            <h1 className="text-2xl font-bold mb-4">Transacciones de la pasarela de pagos HerosPay</h1>
+            <div className="flex flex-row justify-between items-center">
+                <h1 className="text-2xl font-bold">Transacciones de la pasarela de pagos HerosPay</h1>
+                <div className='flex justify-end items-end'>
+                    <Button variant="contained" color="primary" onClick={handleExport} className="mt-4">Exportar a Excel</Button>
+                </div>
+            </div>
             <DataGrid rows={filteredData} columns={columns} pageSize={5} className="mb-4 mt-4" />
-            <Button variant="contained" color="primary" onClick={handleExport} className="mt-4">Exportar a Excel</Button>
         </Box>
+
     );
 };
 
@@ -266,19 +274,181 @@ const Vista = () => {
         navigate('/login');
     };
 
+    const user = {
+        name: 'Tom Cook',
+        email: 'tom@example.com',
+        imageUrl:
+            '/src/assets/images/logo_heros.jpg',
+    }
+    const navigation = [
+        { name: 'Dashboard', href: '#', current: true },
+        { name: 'Team', href: '#', current: false },
+        { name: 'Projects', href: '#', current: false },
+        { name: 'Calendar', href: '#', current: false },
+        { name: 'Reports', href: '#', current: false },
+    ]
+
+    const userNavigation = [
+        /*     { name: 'Your Profile', href: '#', onclick:'' },
+            { name: 'Settings', href: '#',onclick:'' }, */
+        { name: 'Salir', href: '#', onclick: handleLogout },
+    ]
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ')
+    }
+
     return (
-        <div className="container mx-auto p-4">
-            <header className="mb-4">
-                <h2>Bienvenido, {username}</h2>
-                <Button variant="contained" color="secondary" onClick={handleLogout}>
-                    Cerrar sesión
-                </Button>
-            </header>
-            <main>
-                <DataTable />
-            </main>
-        </div>
-    );
+        <>
+
+            <div className="min-h-full">
+                <Disclosure as="nav" className="bg-gray-800">
+                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <div className="flex h-16 items-center justify-between">
+                            <div className="flex items-center">
+                                <div className="shrink-0">
+                                    <img
+                                        alt="Your Company"
+                                        src="/src/assets/images/logo_heros.png"
+                                        className=""
+                                    />
+                                </div>
+                                {/*                                 <div className="hidden md:block">
+                                    <div className="ml-10 flex items-baseline space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                aria-current={item.current ? 'page' : undefined}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium',
+                                                )}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div> */}
+                            </div>
+                            <div className="hidden md:block">
+                                <div className="ml-4 flex items-center md:ml-6">
+                                    <button
+                                        type="button"
+                                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                                    >
+                                        <span className="absolute -inset-1.5" />
+                                        <span className="sr-only">View notifications</span>
+                                        {/* <BellIcon aria-hidden="true" className="size-6" /> */}
+                                        {username}
+                                    </button>
+
+                                    {/* Profile dropdown */}
+                                    <Menu as="div" className="relative ml-3">
+                                        <div>
+                                            <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                                                <span className="absolute -inset-1.5" />
+                                                <span className="sr-only">Open user menu</span>
+                                                <img alt="" src={user.imageUrl} className="size-8 rounded-full" />
+                                            </MenuButton>
+                                        </div>
+                                        <MenuItems
+                                            transition
+                                            className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                                        >
+                                            {userNavigation.map((item) => (
+                                                <MenuItem key={item.name}>
+                                                    <a
+                                                        href={item.href}
+                                                        onClick={item.onclick}
+                                                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                                                    >
+                                                        {item.name}
+                                                    </a>
+                                                </MenuItem>
+                                            ))}
+                                        </MenuItems>
+                                    </Menu>
+                                </div>
+                            </div>
+                            <div className="-mr-2 flex md:hidden">
+                                {/* Mobile menu button */}
+                                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+                                    <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+                                </DisclosureButton>
+                            </div>
+                        </div>
+                    </div>
+
+                    <DisclosurePanel className="md:hidden">
+                        <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+                            {navigation.map((item) => (
+                                <DisclosureButton
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    aria-current={item.current ? 'page' : undefined}
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'block rounded-md px-3 py-2 text-base font-medium',
+                                    )}
+                                >
+                                    {item.name}
+                                </DisclosureButton>
+                            ))}
+                        </div>
+                        <div className="border-t border-gray-700 pt-4 pb-3">
+                            <div className="flex items-center px-5">
+                                <div className="shrink-0">
+                                    <img alt="" src={user.imageUrl} className="size-10 rounded-full" />
+                                </div>
+                                <div className="ml-3">
+                                    <div className="text-base/5 font-medium text-white">{user.name}</div>
+                                    <div className="text-sm font-medium text-gray-400">{user.email}</div>
+                                </div>
+                                <button
+                                    type="button"
+                                    className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+                                >
+                                    <span className="absolute -inset-1.5" />
+                                    <span className="sr-only">View notifications</span>
+                                    <BellIcon aria-hidden="true" className="size-6" />
+                                </button>
+                            </div>
+                            <div className="mt-3 space-y-1 px-2">
+                                {userNavigation.map((item) => (
+                                    <DisclosureButton
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                                    >
+                                        {item.name}
+                                    </DisclosureButton>
+                                ))}
+                            </div>
+                        </div>
+                    </DisclosurePanel>
+                </Disclosure>
+
+                <main>
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+
+                        <div className="container mx-auto p-4">
+                            <main>
+                                <DataTable />
+                            </main>
+                        </div>
+
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+
 };
 
 export default Vista;
