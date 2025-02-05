bfa7b7b0ce12566a29cb9daa6b5053ea9d10431e
Neil Barazarte <ezebarazarte@gmail.com>
2025-02-04 20:04:41 -0400

    avatar en dispositivos pequeños

--- botonHero/src/components/Vista.jsx
+++ botonHero/src/components/Vista.jsx
@@ -404,7 +404,7 @@ const Vista = () => {
                         <div className="border-t border-gray-700 pt-4 pb-3">
                             <div className="flex items-center px-5">
                                 <div className="shrink-0">
-                                    <img alt="" src={user.imageUrl} className="size-10 rounded-full" />
+                                    <img alt="" src={avatar} className="size-10 rounded-full" />
                                 </div>
                                 <div className="ml-3">
                                     <div className="text-base/5 font-medium text-white">{user.name}</div>
