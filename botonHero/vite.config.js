a2e5518fc9ab8aa7d5653f2049d16e59da8ce8b6
Neel <ezebarazarte@gmail.com>
2025-02-12 15:24:15 -0400

    configuracion de vite

--- botonHero/vite.config.js
+++ botonHero/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -6,4 +6,22 @@ export default defineConfig({
   plugins: [react()],
   envPrefix: 'REACT_APP_',
   //base: '/boton/'
+}) */
+
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    {
+      name: 'replace-eval',
+      transform(code) {
+        return code.replace(/eval\(/g, 'safeEval(');
+      },
+    },
+  ],
+  envPrefix: 'REACT_APP_',
+  // base: '/boton/'
 })
